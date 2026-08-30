ab233912a936f2596dfa84c4ebb75903cd96588e
DucAnh72 <153804009+DucAnh72@users.noreply.github.com>
2026-08-30 10:39:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -374,10 +374,9 @@ function startOrder(slotnumber) {
 
         setTimeout(() => {
             if (!currentSign) {
-                console.log("[!] Không tìm thấy sign")
-                };
+                console.log("[!] Không tìm thấy sign");
                 return;
-            };
+            }
 
             bot._client.write("update_sign", {
                 location: currentSign.location,
